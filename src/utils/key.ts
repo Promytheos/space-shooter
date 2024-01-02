30410b791a9f2f6858a5f9febdbd9378d40b23ba
Luke Swan <swan.luke9@gmail.com>
2024-01-02 02:10:48 +0200

    Adding custom event handler

--- src/utils/key.ts
+++ src/utils/key.ts
@@ -3,7 +3,53 @@ interface HandlerConfig {
   release?: Function;
 }
 
-// TODO: Refactor to handle multiple events
+export class EventHandler {
+  private readonly eventMap: Record<string, Array<Function>> = {};
+
+  /**
+   * on - subscribes to an event with an action
+   * @param eventName the name of the event to subscribe to
+   * @param action the action to be executed when the event is fired
+   */
+  public on(eventName: string, action: Function): void {
+    if (!this.eventMap[eventName]) {
+      this.eventMap[eventName] = [];
+    }
+    this.eventMap[eventName].push(action);
+  }
+
+  /**
+   * unsubscribe - unsubscribes a specified action from an event
+   * @param eventName the name of the event that you want to unsubscribe from
+   * @param action the action that you want to remove
+   */
+  public unsubscribe(eventName: string, action: Function): void {
+    const eventIndex = this.eventMap[eventName].indexOf(action);
+    if (eventIndex === -1) {
+      throw new Error(`Listener for ${eventName} does not exist`);
+    }
+    this.eventMap[eventName].splice(eventIndex, 1);
+  }
+
+  /**
+   * unsubscribeAll - unsubscribes all actions attached to an event
+   * @param eventName the name of the event that you want to unsubscribe from
+   */
+  public unsubscribeAll(eventName: string) {
+    this.eventMap[eventName] = [];
+  }
+ 
+  /**
+  * emit - emits an event specified by the user and executes all actions attached to the event
+  * @param eventName the name of the event to be emitted
+  * @param args the arguments to be passed to the actions
+  */
+  public emit(eventName: string, ...args: any[]): void {
+    if (this.eventMap[eventName]) {
+      this.eventMap[eventName].forEach(action => action(...args));
+    }
+  }
+}
 export class KeyHandler {
     readonly value: string;
     isDown: boolean;
